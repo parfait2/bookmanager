6790595d941ffb170fe61e52e69690859174edf0
parfait2 <parkyl0208@naver.com>
2023-10-26 21:19:52 +0900

    [Fix] import version

--- src/main/java/com/example/bookmanager/domain/User.java
+++ src/main/java/com/example/bookmanager/domain/User.java
@@ -1,10 +1,10 @@
 package com.example.bookmanager.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 //@Getter
