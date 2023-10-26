b05abd4ff176e2f78b52c2406a060c0bf366f38a
parfait2 <parkyl0208@naver.com>
2023-10-26 21:19:21 +0900

    [Add] data.sql

---
+++ src/test/resources/data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (1, 'minho', 'haejunkim@metacomedy.net', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (2, 'joon', 'joon@metacomedy.net', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (3, 'coolj', 'coolj@metacomedy.net', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (4, 'wongjong', 'wongjong@metacomedy.net', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (5, 'minho', 'minho@naver.com', now(), now());
