2b8fbc46fd3cdcc0ec15ae20d93c5afd3c78464b
parfait2 <parkyl0208@naver.com>
2023-10-26 21:34:55 +0900

    [Update] CRUD Test

--- src/test/java/com/example/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/example/bookmanager/repository/UserRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.example.bookmanager.repository;
 
 import com.example.bookmanager.domain.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -12,8 +16,39 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
+        /**
+         * test 1
+         * */
+        /*
         userRepository.save(new User());
+        userRepository.findAll().forEach(System.out::println);
 
-        System.out.println(">>>" + userRepository.findAll() );
+//        for(User user : userRepository.findAll()) {
+//            System.out.println(user);
+//        }
+
+         */
+
+        /**
+         * test 2
+         * */
+        /*
+//        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+        List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 3L, 5L));
+        users.forEach(System.out::println);
+         */
+
+        /**
+         * test 3
+         * */
+        User user1 = new User("minho", "dopaminho@naver.com");
+        User user2 = new User("haejun", "haejunkim@metacomedy.net");
+
+//        userRepository.saveAll(Lists.newArrayList(user1, user2));
+        userRepository.save(user1);
+
+        List<User> users = userRepository.findAll();
+
+        users.forEach(System.out::println);
     }
 }
